c3062b40a0adb83f23e6891356bd22e5efbbbc2d
Josep <Josep@priv2g56-154.wi-fi.upv.es>
2017-02-23 18:23:48 +0100

    Created a NetBeans Project
    First commit from the NetBeans project

---
+++ HashCode2017/src/HashCode2017/HashCode2017.java
@@ -0,0 +1,11 @@
+package HashCode2017;
+
+/**
+ * Main class of the project
+ * @author Josep Dols
+ */
+public class HashCode2017 {
+    public static void main(String[] args){
+        System.out.println("HashCode2017");
+    }
+}
